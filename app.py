f929d1ee568d7e80f523c2c033e69be23a0c75f3
Jai Awasthi <115660090+jeezy05@users.noreply.github.com>
2025-12-26 16:51:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -805,4 +805,5 @@ with gr.Blocks(
 
 
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=7860, share=True)
+    demo.launch(server_name="0.0.0.0", server_port=7860, share=True, allowed_paths=["./"])
+
